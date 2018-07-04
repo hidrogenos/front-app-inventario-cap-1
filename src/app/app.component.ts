414e2a37fbd58d1d7ee982e7f5f1241fddffe44e
Unknown <hidrogenos00@gmail.com>
2018-07-04 11:15:57 -0500

    integracion primeng
    integracion primeng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  template: `
+    <div>
+      app component
+      <button pButton type="button" label="Click" ></button>
+    </div>
+  `
 })
 export class AppComponent {
   title = 'app';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { ButtonModule } from 'primeng/button';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, ButtonModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
